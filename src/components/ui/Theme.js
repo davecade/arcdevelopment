b37052fdc42c52fc128332fa44733627bc285fa8
Dave <dave.cadelina@outlook.com>
2021-08-25 23:32:45 +1000

    added material ui theme palette system

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,19 @@
 import { createTheme } from '@material-ui/core/styles';
 
+const arcBlue = "#0B72B9"
+const arcOrange = "#FFBA60"
+
 export default createTheme({
-    
+    palette: {
+        comment: {
+            blue: arcBlue,
+            orange: arcOrange
+        },
+        primary: {
+            main: arcBlue,
+        },
+        secondary: {
+            main: arcOrange
+        }
+    }
 })
