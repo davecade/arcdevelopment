a7038b691d0dabe737f6806a4c1093babfebd889
Dave <dave.cadelina@outlook.com>
2021-08-28 13:32:46 +1000

    added inline styling

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles'
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -15,9 +16,20 @@ function ElevationScroll(props) {
       elevation: trigger ? 4 : 0,
     });
   }
+
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}))
   
 const Header = () => {
+  const classes = useStyles()
+
+
+
     return (
+      <Fragment>
         <ElevationScroll>
             <AppBar position="fixed" color="primary">
                 <Toolbar>
@@ -27,6 +39,8 @@ const Header = () => {
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
+        <div className={classes.toolbarMargin}/>
+      </Fragment>
     )
 }
 
